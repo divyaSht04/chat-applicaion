4f1e46d293954d1b0af32ab253779abde4675a67
divya-icely <divya.shrestha2004@gmail.com>
2026-06-02 11:07:52 +0545

    fix: application start-uo

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
